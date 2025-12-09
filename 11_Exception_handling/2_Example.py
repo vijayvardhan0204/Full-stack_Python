d46bcf650515ee908b8499761010b8bec313c0d1
vijayvardhan0204 <vijay0024official@gmail.com>
2025-12-09 19:13:04 +0530

    codes

---
+++ 11_Exception_handling/2_Example.py
@@ -0,0 +1,11 @@
+try:
+    a = int(input("Enter number: "))
+    print(10 / a)
+except ZeroDivisionError:
+    print("You cannot divide by zero!")
+except ValueError:
+    print("Invalid input!")
+else:
+    print("Successful execution!")
+finally:
+    print("Program ended.")
