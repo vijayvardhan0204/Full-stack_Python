c2ef21493853765d64285e2d0ce541889ec6a50b
vijayvardhan0204 <vijay0024official@gmail.com>
2025-12-09 10:53:54 +0530

    codes

---
+++ 11_Exception_handling/1_Eh(def).py
@@ -0,0 +1,29 @@
+Exception Handling in Python
+
+Exception Handling means managing runtime errors in a program so that the program does not crash and can continue running smoothly.
+
+When Python encounters an error (called an exception), instead of stopping the program, we can handle it using exception-handling blocks.
+
+Python provides these keywords:
+
+1. try
+
+Contains the code that might cause an exception.
+
+2. except
+
+Runs only if an exception occurs inside the try block.
+
+Used to handle the error gracefully.
+
+3. else
+
+Runs only if no exception occurs.
+
+4. finally
+
+Runs always, whether exception occurs or not (commonly used for cleanup tasks like closing files).
+
+5. raise
+
+Used to intentionally raise an exception.
