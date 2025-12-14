26146bb6c4921634b40edf9f705d9fb9424c6432
vijayvardhan0204 <vijay0024official@gmail.com>
2025-12-14 18:44:41 +0530

    codes

--- 4_Dictionary/6_Merging or concatinating.py
+++ 4_Dictionary/6_Merging or concatinating.py
@@ -10,3 +10,15 @@ print(d3)
 # Method 2(Dictionary Unpacking (**))
 d4={**d1,**d2}
 print(d4)
+
+# Method 3 (update())
+d1.update(d2)
+print(d1)
+
+# Method 4
+d5={'name':'vijay','age':20}
+d6={'age':21,'roll':'R3'}
+d7=d5.copy()
+for key,value in d6.items():
+    d7[key]=value
+print(d7)
