73ee74f39580714c097e525d28ed203e3f799a44
vijayvardhan0204 <vijay0024official@gmail.com>
2025-12-15 20:19:58 +0530

    codes

--- 4_Dictionary/5_sum.py
+++ 4_Dictionary/5_sum.py
@@ -1,3 +1,12 @@
+# sum
+
+# Method 1(using sum())
 d = {'a': 100, 'b': 200, 'c': 300}
 res = sum(d.values())
+print(res)
+
+# Method 2(loop)
+res=0
+for value in d.values():
+    res+=value
 print(res)
