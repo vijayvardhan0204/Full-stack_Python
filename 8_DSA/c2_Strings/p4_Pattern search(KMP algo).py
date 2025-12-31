d1540d6d702d718f5d85eb897f1f3c37c1e51bc7
vijayvardhan0204 <vijay0024official@gmail.com>
2025-12-31 12:49:21 +0530

    updated strings

--- 8_DSA/c2_Strings/p4_Pattern search(KMP algo).py
+++ 8_DSA/c2_Strings/p4_Pattern search(KMP algo).py
@@ -1,49 +1,72 @@
-def constructlps(pat,lps):
-    m = len(pat)
+def constructLps(pat, lps):
+    
+    # len stores the length of longest prefix which 
+    # is also a suffix for the previous index
     len_ = 0
+    m = len(pat)
+    
+    # lps[0] is always 0
+    lps[0] = 0
+
     i = 1
-    while i < m :
+    while i < m:
+        
+        # If characters match, increment the size of lps
         if pat[i] == pat[len_]:
             len_ += 1
             lps[i] = len_
             i += 1
-
+        
+        # If there is a mismatch
         else:
             if len_ != 0:
-                len_ = lps[len_-1]
-        
-            else :
+                
+                # Update len to the previous lps value 
+                # to avoid redundant comparisons
+                len_ = lps[len_ - 1]
+            else:
+                
+                # If no matching prefix found, set lps[i] to 0
                 lps[i] = 0
                 i += 1
 
-
-
-
-def search(pat,txt):
+def search(pat, txt):
     n = len(txt)
     m = len(pat)
 
-
     lps = [0] * m
     res = []
-    constructlps(pat,lps)
 
+    constructLps(pat, lps)
+
+    # Pointers i and j, for traversing 
+    # the text and pattern
     i = 0
     j = 0
 
     while i < n:
+        
+        # If characters match, move both pointers forward
         if txt[i] == pat[j]:
             i += 1
             j += 1
 
+            # If the entire pattern is matched 
+            # store the start index in result
             if j == m:
-                res.append(i-j)
-
-                j = lps[j-1]
-
-        else :
+                res.append(i - j)
+                
+                # Use LPS of previous index to 
+                # skip unnecessary comparisons
+                j = lps[j - 1]
+        
+        # If there is a mismatch
+        else:
+            
+            # Use lps value of previous index
+            # to avoid redundant comparisons
             if j != 0:
-                j = lps[j-1]
+                j = lps[j - 1]
             else:
                 i += 1
     return res 
