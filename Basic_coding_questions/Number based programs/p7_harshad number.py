9ff0d11934965ad16ef6232d2b8778f9ed8cf6d3
vijayvardhan0204 <vijay0024official@gmail.com>
2026-01-28 19:47:05 +0530

    updated number based

---
+++ Basic_coding_questions/Number based programs/p7_harshad number.py
@@ -0,0 +1,15 @@
+'''A Harshad number = number divisible by sum of its digits
+Example: 18 → 1 + 8 = 9, 18 % 9 = 0'''
+
+num = int(input("Enter number: "))
+temp = num
+s = 0
+
+while temp > 0:
+    s += temp % 10
+    temp //= 10
+
+if num % s == 0:
+    print("Harshad Number")
+else:
+    print("Not a Harshad Number")
